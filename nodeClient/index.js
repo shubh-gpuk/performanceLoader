c314f25a753b7c27bc2e4d65c31b3dab42e0d46e
shubg-gpuk <shubh.gpuk@gmail.com>
2020-12-01 15:43:08 +0530

    Add file to get performance data from a client machine.

---
+++ nodeClient/index.js
@@ -0,0 +1,92 @@
+//Node program to capture performance data
+
+//Parameters to be captured :
+    // CPU load
+    // Memory usage -
+        // free
+        // total
+    // OS type
+    // Uptime
+    // CPU info - 
+        // Type
+        // Number of cores
+        // Clock speed
+
+const os = require('os');
+const { resolve } = require('path');
+
+
+function performanceData(){
+    return new Promise(async (resolve, reject) => {
+        //Free memory
+        const freeMem = os.freemem();
+
+        //Total memory
+        const totalMem = os.totalmem();
+
+        const memUsage = Math.floor((totalMem - freeMem)*100 / totalMem)/100;
+
+        // OS type
+        let OSType = os.type()
+        if(OSType == 'Darwin')
+            OSType = 'Mac';
+        else if(OSType == 'Windows_NT')
+            OSType = 'Windows';
+
+        // Uptime
+        const upTime = os.uptime();
+
+        //CPU type
+        const cpuType = os.cpus()[0]['model'];
+
+        //CPU number of cores
+        const cpuCores = os.cpus().length;
+
+        //CPU speed
+        const cpuSpeed = os.cpus()[0]['speed'];
+
+        // CPU load
+        const cpuLoad_percent = await getcpuLoad();
+
+
+        //return all data
+        resolve({freeMem, totalMem, memUsage, OSType, upTime, cpuType, cpuCores, cpuSpeed, cpuLoad_percent});
+    });
+}
+
+
+//CPU load
+function cpuAverage(){
+    let idle_ms = 0;
+    let total_ms = 0;
+    const cpus = os.cpus()
+    //iterate over all cores
+    cpus.forEach((core) => {
+        //iterate over every property in "times" object
+        for(type in core.times){
+            total_ms += core.times[type];
+        }
+        idle_ms += core.times.idle;
+    });
+    return {idle : idle_ms / cpus.length,
+            total : total_ms / cpus.length};
+}
+
+//get CPU load after a delay of 100ms
+function getcpuLoad(){
+    return new Promise((resolve, reject) => {
+        const start = cpuAverage();
+        setTimeout(()=> {
+            const end = cpuAverage();
+            const idleDiff_ms = end.idle - start.idle;
+            const totalDiff_ms = end.total - start.total;
+            const cpuLoad_percent = Math.floor((totalDiff_ms - idleDiff_ms)*100 / totalDiff_ms);
+            resolve(cpuLoad_percent);
+        }, 100);
+    });
+}
+
+
+performanceData().then((data) => {
+    console.log(data);
+});
