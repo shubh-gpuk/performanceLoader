b9989cb1ccaeae428d69661e440a8b02c1c85c00
shubg-gpuk <shubh.gpuk@gmail.com>
2020-12-14 13:55:51 +0530

    Show upTime in human readable format.

--- reactclient/src/Info.js
+++ reactclient/src/Info.js
@@ -1,10 +1,15 @@
+import moment from 'moment';
+
 function Info(props) {
+
+    console.log(moment.duration(-1 * props.infoData.upTime, 'seconds').humanize(true));
+
     return (
         <div className="col-sm-3 col-sm-offset-1 cpu-info">
           <h3>Operating System</h3>
           <div className="widget-text">{props.infoData.OSType}</div>
           <h3>Time Online</h3>
-          <div className="widget-text">{props.infoData.upTime}</div>
+          <div className="widget-text">{moment.duration(-1 * props.infoData.upTime, 'seconds').humanize(true)}</div>
           <h3>Processor information</h3>
           <div className="widget-text"><strong>Type:</strong> {props.infoData.cpuType}</div>
           <div className="widget-text"><strong>Number of Cores:</strong> {props.infoData.cpuCores}</div>
